a193a9959a69a2da5b115fc800f538b6da36f49e
Thomas Le Bihan <thomas.66days@gmail.com>
2021-05-04 16:30:08 +0200

    save for ticket

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -32,39 +32,41 @@ const Cart = ({ cartLines, setCartLines }) => {
     </div>
   ) : (
     <div className="cart">
-      <button className="cart-cta">Valider mon panier</button>
-      {uniqueCartLines.map((line) => {
-        return (
-          <div key={line.id} className="cart-line">
-            <div className="cart-line-counter">
-              <Counter
-                cartLines={cartLines}
-                setCartLines={setCartLines}
-                line={line}
-              />
-              <span className="cart-line-counter--title">{line.title}</span>
+      <div className="cart-container">
+        <button className="cart-cta">Valider mon panier</button>
+        {uniqueCartLines.map((line) => {
+          return (
+            <div key={line.id} className="cart-line">
+              <div className="cart-line-counter">
+                <Counter
+                  cartLines={cartLines}
+                  setCartLines={setCartLines}
+                  line={line}
+                />
+                <span className="cart-line-counter--title">{line.title}</span>
+              </div>
+              <span className="cart-line-price">
+                {line.price.replace(".", ",")} €
+              </span>
             </div>
-            <span className="cart-line-price">
-              {line.price.replace(".", ",")} €
-            </span>
+          );
+        })}
+
+        <div className="cart-sub-total">
+          <div className="cart-sub-total--sum">
+            <span>Sous-total</span>
+            <span>{subTotal().replace(".", ",")} €</span>
           </div>
-        );
-      })}
-
-      <div className="cart-sub-total">
-        <div className="cart-sub-total--sum">
-          <span>Sous-total</span>
-          <span>{subTotal().replace(".", ",")} €</span>
+          <div className="cart-sub-total--fee">
+            <span>Frais de livraison</span>
+            <span>{shippingFee.toFixed(2).replace(".", ",")} €</span>
+          </div>
         </div>
-        <div className="cart-sub-total--fee">
-          <span>Frais de livraison</span>
-          <span>{shippingFee.toFixed(2).replace(".", ",")} €</span>
-        </div>
-      </div>
 
-      <div className="cart-total">
-        <span>Total</span>
-        <span>{total(shippingFee).replace(".", ",")} €</span>
+        <div className="cart-total">
+          <span>Total</span>
+          <span>{total(shippingFee).replace(".", ",")} €</span>
+        </div>
       </div>
     </div>
   );
